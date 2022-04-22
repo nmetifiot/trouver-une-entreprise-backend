7de17debc7d081ac5de5a41e8ff0b53e421a7ebb
Nicolas Métifiot <metifiot.nicolas@gmail.com>
2022-04-22 13:56:05 +0200

    ajout des messages d erreurs sur la request

--- controllers/bonneBoite.js
+++ controllers/bonneBoite.js
@@ -11,28 +11,45 @@ const getToken = require('../outils/auth');
 exports.getEntrepriseCodeInseeCodeRome = async (req, res, next) => {
 
     tokenUsed = await getToken();
+    isRequestOk = true;
+    messageErreur = {};
 
-    try {
-        reponse = await axios.get('https://api.emploi-store.fr/partenaire/labonneboite/v1/company/', {
-            params: {
-                commune_id: req.query.codeInsee,
-                rome_codes: req.query.codeRome
-            },
-            headers: {
-                Authorization: `Bearer ${tokenUsed}`
-            }
-        });
-        res.status(reponse.status).json({
-            entreprises: reponse.data.companies,
-            nombre_entreprises: reponse.data.companies_count,
-            rome_code: reponse.data.rome_code,
-            rome_label: reponse.data.rome_label,
-            url: reponse.data.url
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        })
+    if (!req.query.codeInsee) {
+        messageErreur["codeInsee"] = "champ codeInsee obligatoire, le code insee de la ville est obligatoire";
+        isRequestOk = false;
     };
+    if (!req.query.codeRome) {
+        messageErreur["codeRome"] = "champ codeRome obligatoire, le code métier recherché est obligatoire";
+        isRequestOk = false;
+    };
+
+
+    if (isRequestOk) {
+        try {
+
+            reponse = await axios.get('https://api.emploi-store.fr/partenaire/labonneboite/v1/company/', {
+                params: {
+                    commune_id: req.query.codeInsee,
+                    rome_codes: req.query.codeRome
+                },
+                headers: {
+                    Authorization: `Bearer ${tokenUsed}`
+                }
+            });
+            res.status(reponse.status).json({
+                entreprises: reponse.data.companies,
+                nombre_entreprises: reponse.data.companies_count,
+                rome_code: reponse.data.rome_code,
+                rome_label: reponse.data.rome_label,
+                url: reponse.data.url
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error
+            })
+        };
+    } else {
+        res.status(500).json(messageErreur);
+    }
 
 };
